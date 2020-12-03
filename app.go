29c0c14df77b5c4596445485db46cb2ea87e51cf
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-12-03 01:20:42 +0000

    done in a way

--- app.go
+++ app.go
@@ -5,53 +5,93 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"log"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 var (
-	count   int = 0
-	correct int = 0
+	sc = bufio.NewReader(os.Stdin)
 )
 
-func main() {
-	fmt.Println("Quiz game")
+func checkResponse(s string, answer string) bool {
+	return strings.TrimSpace(s) == strings.TrimSpace(answer)
+}
 
-	// flags
-	csvPtr := flag.String("csv", "./problems.csv", "path to csv file")
+type quiz struct {
+	questions [][]string
+	correct   int
+}
 
-	flag.Parse()
-
-	f, err := os.Open(*csvPtr)
+// LoadQuestions loads questions from a csv file
+func (q *quiz) LoadQuestions(filename string, shuffle bool) {
+	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	sc := bufio.NewReader(os.Stdin)
 	r := csv.NewReader(f)
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
+	records, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
 
-		count++
-		fmt.Printf("Problem #%d: %s = ", count, record[0])
-		response, err := sc.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
+	if shuffle {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(records), func(i, j int) {
+			records[i], records[j] = records[j], records[i]
+		})
+	}
+	q.questions = records
+}
 
-		if strings.TrimSpace(response) == strings.TrimSpace(record[1]) {
-			correct++
+// Start handles gameplay
+func (q *quiz) Start(seconds int) {
+	fmt.Println("Quiz game")
+	fmt.Print("Press Enter/Return to begin...")
+	_, err := sc.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	timer := time.NewTimer(time.Duration(seconds) * time.Second)
+
+	for index, question := range q.questions {
+		fmt.Printf("Problem #%d: %s = ", index+1, question[0])
+		c := make(chan string)
+		go func() {
+			response, err := sc.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+			c <- response
+		}()
+
+		select {
+		case <-timer.C:
+			println()
+			goto Announce
+		case response := <-c:
+			if checkResponse(response, question[1]) {
+				q.correct++
+			}
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d.\n", correct, count)
+Announce:
+	fmt.Printf("You scored %d out of %d.\n", q.correct, len(q.questions))
+}
+
+func main() {
+	// flags
+	csvPtr := flag.String("csv", "./problems.csv", "path to csv file in the format `question,answer` (defaults to problems.csv)")
+	limitPtr := flag.Int("limit", 30, "time limit for the quiz in seconds")
+	shufflePtr := flag.Bool("shuffle", false, "shuffle problems")
+	flag.Parse()
+
+	q := &quiz{}
+	q.LoadQuestions(*csvPtr, *shufflePtr)
+	q.Start(*limitPtr)
 }
